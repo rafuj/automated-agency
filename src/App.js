afcbc2c9ad4325cbdb6882ce41a6c3187075406f
rafuj6amtech <rafuj@6amtech.net>
2022-12-16 22:57:29 +0600

    Initialize React Router Dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignIn from "./pages/auth/SignIn";
+import SignUp from "./pages/auth/SignUp";
+
 function App() {
-	return <div className="App"></div>;
+	return (
+		<>
+			<BrowserRouter>
+				<Routes>
+					<Route path="auth">
+						<Route index element={<SignIn />} />
+						<Route path="signin" element={<SignIn />} />
+						<Route path="signup" element={<SignUp />} />
+					</Route>
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
 }
 
 export default App;
