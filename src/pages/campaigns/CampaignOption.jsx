1d8b430b5967a3f425e3026f1332875d9ac6d7f5
rafuj <raihanrafuj9978@gmail.com>
2022-12-22 07:02:23 +0600

    Campaign Option Page Added

--- src/pages/campaigns/CampaignOption.jsx
+++ src/pages/campaigns/CampaignOption.jsx
@@ -1,8 +1,26 @@
 import React from "react";
+import FormGroup from "../../components/FormGroup";
 import CampaignSingleLayout from "./components/CampaignSingleLayout";
 
 const CampaignOption = () => {
-	return <CampaignSingleLayout></CampaignSingleLayout>;
+	return (
+		<CampaignSingleLayout>
+			<div className="row">
+				<div className="col-sm-6">
+					<FormGroup label="Accounts to use" type="text" />
+				</div>
+				<div className="col-sm-6">
+					<FormGroup label="Daily Message Limit" type="text" />
+				</div>
+				<div className="col-sm-12 pt-2">
+					<div className="d-flex flex-wrap justify-content-center save-btn-grp">
+						<button className="cmn-btn btn-outline">Save</button>
+						<button className="cmn-btn">Launch</button>
+					</div>
+				</div>
+			</div>
+		</CampaignSingleLayout>
+	);
 };
 
 export default CampaignOption;
